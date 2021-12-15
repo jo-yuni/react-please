188aff31fe3d14629a088ae3033246206e57bba6
jo-yuni <84690460+jo-yuni@users.noreply.github.com>
2021-12-15 10:59:07 +0900

    으으응... 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import { element } from "prop-types";
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/id" exact component={Detail}>
+        <Route path="/:id" element={<Detail />}>
         </Route>
-        <Route path="/" exact component={Home}>
+        <Route path="/" element={<Home />}>
         </Route>
-      </Routes>
-    </Router>
+      </Routes >
+    </Router >
   );
 }
 
